0ce1aa531414fca0f48a16674bdb7819a9f2d0af
paulorac <paulmartinorac@gmail.com>
2020-02-26 04:17:17 +0000

    Updated source to fit new requirements

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Local Authentication with Passport and Mongo
 
-- clone the repo
-- npm install
-- Install MongoDB
-- Run mongod locally
-- Using Mongo cli, create a db *MyDatabase* with a collection *userInfo*.
-- Add a few users to your collection with fields *username* and *password*
-- Run the app with: node index.js
-- navigate to localhost:3000 and login with one of the credentials you created.
+* clone the repo
+* npm install
+* Install [MongoDB](https://www.mongodb.com/download-center#community) 
+* Using Mongo cli, create a db *MyDatabase*.
+* Run the app with: node index.js
+* Comment everything after line 97 in index.js.
+* Navigate to localhost:3000 and login with one of the credentials listed below.
+
+### Test Credentials
+
+username: paul
+password: paul
+
+username: jay
+password: jay
+
+username: roy
+password: roy
--- index.js
+++ index.js
@@ -3,13 +3,21 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
 
 const port = process.env.PORT || 3000;
-app.listen(port , () => console.log('App listening on port ' + port));
+app.listen(port, () => console.log('App listening on port ' + port));
 
 /*  PASSPORT SETUP  */
 
@@ -17,58 +25,77 @@ const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/success', (req, res) => res.send("Welcome "+ req.query.username + "!!"));
-app.get('/error', (req, res) => res.send("error logging in"));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(id, cb) {
-  User.findById(id, function(err, user) {
-    cb(err, user);
-  });
-});
-
 /* MONGOOSE SETUP */
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/MyDatabase');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://localhost/MyDatabase', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
 const UserDetail = new Schema({
-      username: String,
-      password: String
-    });
-const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo3', UserDetail, 'userInfo3');
 
 /* PASSPORT LOCAL AUTHENTICATION */
 
-const LocalStrategy = require('passport-local').Strategy;
+passport.use(UserDetails.createStrategy());
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-      UserDetails.findOne({
-        username: username 
-      }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
 
-        if (!user) {
-          return done(null, false);
-        }
+/* ROUTES */
 
-        if (user.password != password) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
-  }
-));
+connectEnsureLogin = require('connect-ensure-login');
 
-app.post('/', 
-  passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-    res.redirect('/success?username='+req.user.username);
-  });
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) { 
+      return res.redirect('/login?info=' + info); 
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+app.get('/login',
+  (req, res) => res.sendFile('html/login.html',
+  { root: __dirname })
+);
+
+app.get('/',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/index.html', {root: __dirname})
+);
+
+app.get('/private',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/private.html', {root: __dirname})
+);
+
+app.get('/user',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+/* REGISTER SOME USERS */
+
+UserDetails.register({username:'paul', active: false}, 'paul');
+UserDetails.register({username:'jay', active: false}, 'jay');
+UserDetails.register({username:'roy', active: false}, 'roy');
